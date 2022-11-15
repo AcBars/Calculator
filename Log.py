76238822966cd544bad4404e97f7750c8199ab9d
Artem_notebook <mobail_2003@bk.ru>
2022-11-16 01:05:36 +0700

    Добавленно логгирвание пользователя

--- Log.py
+++ Log.py
@@ -1,17 +1,20 @@
 from datetime import datetime as dt
+import getpass
 
 def log_decision (exp, decisin):
     time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
     with open('Calculator\log.csv', 'a') as file:
-        file.write(f'\n {time}; {exp}; {decisin}')
+        file.write(f'\n {time}; {exp}; {decisin}; {getpass.getuser()}')
 
 
 def log_startend (status):
     if status == 1:
         time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
         with open('Calculator\log.csv', 'a') as file:
-            file.write(f'\n {time}; Start')
+            file.write(f'\n {time}; Start; {getpass.getuser()}')
     else:
         time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
         with open('Calculator\log.csv', 'a') as file:
-            file.write(f'\n {time}; End')
+            file.write(f'\n {time}; End; {getpass.getuser()}')
+            
+
