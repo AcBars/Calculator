02518ffdc4ed06dae5f2aceca745fa0ae83aebe1
Artem_notebook <mobail_2003@bk.ru>
2022-11-16 01:31:35 +0700

    Устранены замечания

--- Log.py
+++ Log.py
@@ -1,20 +1,20 @@
 from datetime import datetime as dt
 import getpass
 
-def log_decision (exp, decisin):
+def log_solution (exp, decisin):
     time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
     with open('Calculator\log.csv', 'a') as file:
-        file.write(f'\n {time}; {exp}; {decisin}; {getpass.getuser()}')
+        file.write(f'\n {time}; {exp}; {decisin}; calculation; {getpass.getuser()}')
 
 
 def log_startend (status):
     if status == 1:
         time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
         with open('Calculator\log.csv', 'a') as file:
-            file.write(f'\n {time}; Start; {getpass.getuser()}')
+            file.write(f'\n {time}; ; ; Start; {getpass.getuser()}')
     else:
         time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
         with open('Calculator\log.csv', 'a') as file:
-            file.write(f'\n {time}; End; {getpass.getuser()}')
+            file.write(f'\n {time}; ; ; End; {getpass.getuser()}')
             
 
