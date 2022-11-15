e66e60d683cdd1b75e21eaf97d4dc53a21f07851
psyfluid <psyfluid@gmail.com>
2022-11-15 22:35:05 +0300

    log.py: update log path, names and log order

--- log.py
+++ log.py
@@ -1,20 +1,20 @@
 from datetime import datetime as dt
 import getpass
 
-def log_solution (exp, decisin):
+def log_solution (exp, solution):
     time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
-    with open('Calculator\log.csv', 'a') as file:
-        file.write(f'\n {time}; {exp}; {decisin}; calculation; {getpass.getuser()}')
+    with open('log.csv', 'a') as file:
+        file.write(f'\n {time}; calculation; {getpass.getuser()}; {exp}; {solution}')
 
 
 def log_startend (status):
     if status == 1:
         time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
-        with open('Calculator\log.csv', 'a') as file:
-            file.write(f'\n {time}; ; ; Start; {getpass.getuser()}')
+        with open('log.csv', 'a') as file:
+            file.write(f'\n {time}; Start; {getpass.getuser()}')
     else:
         time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
-        with open('Calculator\log.csv', 'a') as file:
-            file.write(f'\n {time}; ; ; End; {getpass.getuser()}')
+        with open('log.csv', 'a') as file:
+            file.write(f'\n {time}; End; {getpass.getuser()}')
             
 
