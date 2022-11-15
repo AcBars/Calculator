3b9429011195b9ad3de3abb693688f5e23e8073d
psyfluid <psyfluid@gmail.com>
2022-11-15 22:28:22 +0300

    log.py: update filename and log path

--- log.py
+++ log.py
@@ -3,18 +3,18 @@ import getpass
 
 def log_decision (exp, decisin):
     time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
-    with open('Calculator\log.csv', 'a') as file:
+    with open('log.csv', 'a') as file:
         file.write(f'\n {time}; {exp}; {decisin}; {getpass.getuser()}')
 
 
 def log_startend (status):
     if status == 1:
         time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
-        with open('Calculator\log.csv', 'a') as file:
+        with open('log.csv', 'a') as file:
             file.write(f'\n {time}; Start; {getpass.getuser()}')
     else:
         time = dt.now().strftime('%d.%m.%Y ; %H:%M:%S')
-        with open('Calculator\log.csv', 'a') as file:
+        with open('log.csv', 'a') as file:
             file.write(f'\n {time}; End; {getpass.getuser()}')
             
 
